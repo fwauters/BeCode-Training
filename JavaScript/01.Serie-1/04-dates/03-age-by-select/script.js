2c33b573e381b34433e933aee6d1d5fc991fc2b4
François Sin <fwauters13@gmail.com>
2020-07-10 09:32:02 +0200

    ex4 serie1

--- JavaScript/01.Serie-1/04-dates/03-age-by-select/script.js
+++ JavaScript/01.Serie-1/04-dates/03-age-by-select/script.js
@@ -5,5 +5,34 @@
 (() => {
 
     // your code here
+    document.getElementById("run").addEventListener("click", () => {
+
+        let dateObj = new Date();
+        let dayA = dateObj.getDate();
+        let monthA = dateObj.getMonth() + 1; //months from 1-12
+        let yearA = dateObj.getFullYear();
+
+        let actualDate = dayA + "/" + monthA + "/" + yearA;
+        console.log(actualDate);
+
+        let d = document.getElementById("dob-day");
+        let dayU = d.options[d.selectedIndex].value;
+        let m = document.getElementById("dob-month");
+        let monthU = m.options[m.selectedIndex].value;
+        let y = document.getElementById("dob-year");
+        let yearU = y.options[y.selectedIndex].value;
+
+        console.log(dayU + "/" + monthU + "/" + yearU);
+
+        let daysA = (yearA * 365.25) + (monthA * 30.42) + dayA;
+        console.log(daysA);
+        
+        let daysU = (yearU * 365.25) + (monthU * 30.42) + dayU;
+        console.log(daysU);
+
+        let result = daysA - daysU;
+        let ageU = Number(Math.floor(result / 365.25));
+        alert("You are " + ageU + " years old.");
+    }); 
 
 })();
--- JavaScript/01.Serie-1/04-dates/04-locale-date/script.js
+++ JavaScript/01.Serie-1/04-dates/04-locale-date/script.js
@@ -7,5 +7,83 @@
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
 
     // your code here
+    let dateObj = new Date();
+    let minute = dateObj.getMinutes();
+    let hour = dateObj.getHours();
+    let day = String(dateObj.getDay());
+    let date = dateObj.getDate();
+    let month = String(dateObj.getMonth());
+    let year = dateObj.getFullYear();
+
+    let actualDate = day + "/" + month + "/" + year + " " + hour + ":" + minute;
+        console.log(actualDate);
+
+    let textDay;
+    switch (day) {
+        case '0' :
+            textDay = "Sunday";
+            break;
+        case '1' :
+            textDay = "Monday";
+            break;
+        case '2' :
+            textDay = "Tuesday";
+            break;
+        case '3' :
+            textDay = "Wednesday";
+            break;
+        case '4' :
+            textDay = "Thursday";
+            break;
+        case '5' :
+            textDay = "Friday";
+            break;
+        case '6' :
+            textDay = "Saturday";
+            break;        
+    }
+
+    let textMonth;
+    switch (month) {
+        case '0' :
+            textMonth = "January";
+            break;
+        case '1' :
+            textMonth = "February";
+            break;
+        case '2' :
+            textMonth = "March";
+            break;
+        case '3' :
+            textMonth = "April";
+            break;
+        case '4' :
+            textMonth = "May";
+            break;
+        case '5' :
+            textMonth = "June";
+            break;
+        case '6' :
+            textMonth = "July";
+            break;
+        case '7' :
+            textMonth = "August";
+            break;
+        case '8' :
+            textMonth = "September";
+            break;
+        case '9' :
+            textMonth = "October";
+            break;
+        case '10' :
+            textMonth = "November";
+            break;
+        case '11' :
+            textMonth = "December";
+            break;
+    }
+
+    document.getElementById("target").innerHTML =
+        textDay + " " + date + " " + textMonth + " " + year + ", " + hour + "h" + minute;
 
 })();
