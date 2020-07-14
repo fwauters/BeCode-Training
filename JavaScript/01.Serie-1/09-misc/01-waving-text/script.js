a91b954a32a7b54af5fff78170d328f9d17ec142
François Sin <fwauters13@gmail.com>
2020-07-14 18:49:52 +0200

    trying to update js ex9.1

--- JavaScript/01.Serie-1/09-misc/01-waving-text/script.js
+++ JavaScript/01.Serie-1/09-misc/01-waving-text/script.js
@@ -5,27 +5,55 @@
 
     // your code here
 
-    function animation() { 
+    function fontSizeWave(elemHtml) { 
         let x = 2;
-        let y = 10;
-        let id = setInterval(frame, 100);
+        let y = 8;
+        let int = setInterval(frame, 100);
         function frame() {
-            if (x === 10) {
+            if (x === 8) {
                 if (y === 1) {
-                    clearInterval(id);
+                    clearInterval(int);
                 }
                 else {
-                    document.getElementById("target").style.fontSize = y + "rem";
+                    elemHtml.style.fontSize = y + "rem";
                     y--;
                 }               
             }
             else { 
-            document.getElementById("target").style.fontSize = x + "rem";
-            x++;
+                elemHtml.style.fontSize = x + "rem";
+                x++;
             }
         }
     }
 
-    animation();
+    function selCharWithDelay(txt) {
+        let char;
+        let i = 0;
+        let int = setInterval(frame, 100);
+        function frame() {
+            if (i === txt.length) {
+                clearInterval(int);
+            }
+            else {
+               char = txt.substring(i, (i + 1));
+               console.log(char);
+                i++;
+            }
+        }
+    }
+    
+    //-----------document.getElementsByTagName(tag).remove();-----------
+    function addTag(string, tag) {
+        let result = tag + string + tag;
+        return result;
+    }
+    /*
+    let txt = document.getElementById("target").textContent;
+    console.log(txt);
 
+    console.log(selCharWithDelay(txt));
+    */
+    fontSizeWave(document.getElementById("target"));
+
+    selCharWithDelay(document.getElementById("target").textContent);
 })();
