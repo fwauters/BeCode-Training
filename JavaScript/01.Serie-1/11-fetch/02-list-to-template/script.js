1c23d1fd76fede38efc9bd7075ad955e9ce926ab
fwauters <fwauters13@gmail.com>
2020-07-15 19:35:04 +0200

    js start ex11 & update 9.1

--- JavaScript/01.Serie-1/11-fetch/02-list-to-template/script.js
+++ JavaScript/01.Serie-1/11-fetch/02-list-to-template/script.js
@@ -2,4 +2,15 @@
 
 (() => {
     // your code here
+    document.getElementById("run").addEventListener("click", () => {
+        fetch("http://localhost:3000/heroes").then((response) => {
+            return response.json();
+        })
+            .then((response) => {
+                console.log(response);
+                return response;
+            }).then((response) => {
+                console.log(response[1].name);
+            })
+    })
 })();
