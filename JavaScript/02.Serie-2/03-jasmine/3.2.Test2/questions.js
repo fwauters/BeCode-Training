f9043b873dbc7609079668712c4d41274a056b24
François Sin <fwauters13@gmail.com>
2020-07-27 13:15:47 +0200

    js ex3.2 serie2

--- JavaScript/02.Serie-2/03-jasmine/3.2.Test2/questions.js
+++ JavaScript/02.Serie-2/03-jasmine/3.2.Test2/questions.js
@@ -1,43 +1,50 @@
 let CreationTableauLangages =  () => {
-
+    let langages = ["Html","CSS","Java","PHP"];
+    return langages;
 }
 
 let CreationTableauNombres =  () => {
-
+    let numbers = [0,1,2,3,4,5];
+    return numbers;
 }
 
 let RemplacementElement =  (langages) => {
-
+    langages.splice(2, 1, "Javascript");
+    return langages;
 }
 
 let AjoutElementLangages =  (langages) => {
-
+    langages.push("Ruby", "Python");
+    return langages;
 }
 
 let AjoutElementNombres =  (nombres) => {
-
+    nombres.unshift(-2, -1);
+    return nombres;
 }
 
 let SuppressionPremierElement =  (langages) => {
-
+    langages.splice(0, 1);
+    return langages;
 }
 
 let SuppressionDernierElement =  (langages) => {
-
+    langages.pop();
+    return langages;
 }
 
 let ConversionChaineTableau =  (reseaux_sociaux_chaine) => {
-
+    let reseaux_sociaux = reseaux_sociaux_chaine.split(",");
+    return reseaux_sociaux;
 }
-
 let ConversionTableauChaine =  (langages) => {
-
+    return langages.join();
 }
 
 let TriTableau =  (reseaux_sociaux) => {
-
+    return reseaux_sociaux.sort();
 }
 
-let InversionTableau =  (reseaux_sociaux) =>{
-
+let InversionTableau =  (reseaux_sociaux) => {
+    return reseaux_sociaux.reverse();
 }
